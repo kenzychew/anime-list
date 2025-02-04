1c131fa39d4a566d7f548be95f949dc9e4f63ddf
kenzychew <kenzychew@gmail.com>
2025-02-04 08:44:51 +0800

    feat: README

--- src/components/Button/ThemeButton.jsx
+++ src/components/Button/ThemeButton.jsx
@@ -4,7 +4,7 @@ import '../../styles/ThemeButton.css';
 // Custom button component that toggles the theme
 const ThemeButton = () => {
   const { darkMode, toggleTheme } = useTheme(); // Use useTheme hook to access context values
-  // Render a button that calls toggleTheme when clicked
+  // 
   return (
     <button onClick={toggleTheme} className="theme-toggle">
       {darkMode ? '☀️' : '🌙'}
--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,10 +1,11 @@
 import { createContext, useState, useContext } from 'react';
 // Creates React Context for theme-related data
-const ThemeContext = createContext();
-// ThemeProvider component maintains the state of the theme using useState and provides a function toggleTheme, App.jsx wraps children in ThemeProvider
+const ThemeContext = createContext(); //? Think of this like a global "container" that holds data (state of the theme) accessible throughout app
+// ThemeProvider component
 export const ThemeProvider = ({ children }) => {
-  const [darkMode, setDarkMode] = useState(false); // False = Light Mode
-
+  // State lifted to provider level
+  const [darkMode, setDarkMode] = useState(false); //? False = Light Mode
+  // Theme toggle function is also lifted
   const toggleTheme = () => {
     setDarkMode(!darkMode); // Flips the state
     document.documentElement.classList.toggle('dark-mode'); // Toggles the dark-mode class on the document element
@@ -12,11 +13,17 @@ export const ThemeProvider = ({ children }) => {
   // Provides the theme state and toggle function to the children
   return (
     <ThemeContext.Provider value={{ darkMode, toggleTheme }}>
-      {children}
+      {children} {/* This represents all components wrapped by ThemeProvider */}
     </ThemeContext.Provider>
   );
 };
 // Custom hook to use the theme context
-export const useTheme = () => useContext(ThemeContext); 
+export const useTheme = () => useContext(ThemeContext);
+
+// The children prop allows ThemeProvider to:
+// 1. Wrap any components
+// 2. Make theme data available to them
+// 3. Render them inside the Provider
+// 4. Avoid prop drilling through intermediate components
 
 //? https://medium.com/@riteshbhagat/implementing-theme-in-react-using-context-api-196149967c9d
