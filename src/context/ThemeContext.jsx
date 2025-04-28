a12ce7d34b2f87d13a5c4837650c0a216d689b8e
kenzychew <kenzychew@gmail.com>
2025-04-28 12:06:52 +0800

    default dark

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,14 +1,23 @@
-import { createContext, useState, useContext } from 'react';
+import { createContext, useState, useContext, useEffect } from 'react';
 // Creates React Context for theme-related data
 const ThemeContext = createContext(); //? Think of this like a global "container" that holds data (state of the theme) accessible throughout app
 // ThemeProvider component
 export const ThemeProvider = ({ children }) => {
   // State lifted to provider level
-  const [darkMode, setDarkMode] = useState(false); //? False = Light Mode
+  const [darkMode, setDarkMode] = useState(true); // Set default to true for dark mode
+
+  useEffect(() => {
+    // Apply dark mode class to document based on state
+    if (darkMode) {
+      document.documentElement.classList.add('dark-mode');
+    } else {
+      document.documentElement.classList.remove('dark-mode');
+    }
+  }, [darkMode]);
+
   // Theme toggle function is also lifted
   const toggleTheme = () => {
     setDarkMode(!darkMode); // Flips the state
-    document.documentElement.classList.toggle('dark-mode'); // Toggles the dark-mode class on the document element
   };
   // Provides the theme state and toggle function to the children
   return (
